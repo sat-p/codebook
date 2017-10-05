cfbf00e92b3a5f1c23532675fd0637655f20a2ec
Satya Prakash Panuganti <prakashsatya10@gmail.com>
2017-10-05 18:14:24 +0530

    Modified Fenwick Tree

--- BIT.cpp
+++ BIT.cpp
@@ -1,58 +1,66 @@
-// Binary indexed tree supporting binary search.
-struct BIT {
+#include <bits/stdc++.h>
+
+// Fenwick Tree
+struct BIT
+{   
+    typedef int dt;
     int n;
-    vector<int> bit;
-    // BIT can be thought of as having entries f[1], ..., f[n]
-    // which are 0-initialized
-    BIT(int n):n(n), bit(n+1) {}
-    // returns f[1] + ... + f[idx-1]
-    // precondition idx <= n+1
-    int read(int idx) {
-        idx--;
-        int res = 0;
+    std::vector<dt> bit;
+    BIT (int n) : n (n), bit (n + 1) {}
+    
+    dt read (int idx) {
+    
+        dt res = 0;
         while (idx > 0) {
+        
             res += bit[idx];
-            idx -= idx & -idx;
+            idx -= idx & (-idx);
         }
+        
         return res;
     }
-    // returns f[idx1] + ... + f[idx2-1]
-    // precondition idx1 <= idx2 <= n+1
-    int read2(int idx1, int idx2) {
-        return read(idx2) - read(idx1);
-    }
-    // adds val to f[idx]
-    // precondition 1 <= idx <= n (there is no element 0!)
-    void update(int idx, int val) {
+    
+    void update (int idx, int val)
+    {
         while (idx <= n) {
+        
             bit[idx] += val;
-            idx += idx & -idx;
+            idx += idx & (-idx);
         }
     }
-    // returns smallest positive idx such that read(idx) >= target
-    int lower_bound(int target) {
-        if (target <= 0) return 1;
-        int pwr = 1; while (2*pwr <= n) pwr*=2;
-        int idx = 0; int tot = 0;
-        for (; pwr; pwr >>= 1) {
-            if (idx+pwr > n) continue;
-            if (tot + bit[idx+pwr] < target) {
-                tot += bit[idx+=pwr];
-            }
+    
+    /*
+     * Below code may not be required for most tasks.
+     */
+    
+    // Similar to STL's lower bound function
+    int lower_bound (dt target)
+    {
+        if (target <= 0) return 0;
+        dt pwr = 1;
+        while (pwr * 2 <= n) pwr *= 2;
+        int idx = 0;
+        dt tot = 0;
+        while (pwr) {
+            if (idx + pwr <=  n && tot + bit[idx + pwr] < target)
+                tot += bit[idx += pwr];
+            pwr >>= 1;
         }
-        return idx+2;
+        return idx + 1;
     }
-    // returns smallest positive idx such that read(idx) > target
-    int upper_bound(int target) {
-        if (target < 0) return 1;
-        int pwr = 1; while (2*pwr <= n) pwr*=2;
-        int idx = 0; int tot = 0;
-        for (; pwr; pwr >>= 1) {
-            if (idx+pwr > n) continue;
-            if (tot + bit[idx+pwr] <= target) {
-                tot += bit[idx+=pwr];
-            }
+    
+    // Similar to STL's upper bound function
+    int upper_bound (dt target)
+    {
+        if (target <= 0) return 0;
+        dt pwr = 1;
+        while (pwr * 2 <= n) pwr *= 2;
+        int idx = 0;
+        dt tot = 0;
+        while (pwr) {
+            if (idx + pwr <= n && tot + bit[idx + pwr] <= target) // <= instead of <
+                tot += bit[idx += pwr];
+            pwr >>= 1;
         }
-        return idx+2;
     }
 };
